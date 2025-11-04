bbc76f8f2e685fe811f856dd2a95c047c8769e25
Yermukhamed Shakhman <ermukhamed.shakhman@gmail.com>
2025-11-04 15:36:06 +0100

    optimized imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About'; 
-import Join from './pages/Join-us';
-import Events from './pages/Events';
-import Partners from './pages/Partners';
+import React, { lazy, Suspense } from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+// Lazy-load route components for better performance (code-splitting)
+const Home = lazy(() => import('@pages/Home'))
+const About = lazy(() => import('@pages/About'))
+const Join = lazy(() => import('@pages/Join-us'))
+const Events = lazy(() => import('@pages/Events'))
+const Partners = lazy(() => import('@pages/Partners'))
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/join" element={<Join />} />
-      <Route path="/events" element={<Events />} />
-      <Route path="/partners" element={<Partners />} />
-    </Routes>
-  );
+    <Suspense
+      fallback={
+        <div className="w-full min-h-[40vh] flex items-center justify-center text-white/70">
+          Loading…
+        </div>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/partners" element={<Partners />} />
+      </Routes>
+    </Suspense>
+  )
 }
