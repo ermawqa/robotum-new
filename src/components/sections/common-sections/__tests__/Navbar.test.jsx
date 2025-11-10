a139be1baa6dbf87f4b38b92614ff9ce1dc405f8
Yermukhamed Shakhman <ermukhamed.shakhman@gmail.com>
2025-11-10 21:56:12 +0100

    integrated tests

---
+++ src/components/sections/common-sections/__tests__/Navbar.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import Navbar from '../Navbar'
+
+test('highlights active route', () => {
+  render(<Navbar />, { wrapper: ({children}) =>
+    <MemoryRouter initialEntries={['/partners']}>{children}</MemoryRouter>
+  })
+  const partners = screen.getByRole('link', { name: /partners/i })
+  expect(partners).toHaveClass('text-indigo-300') // your active class
+})
