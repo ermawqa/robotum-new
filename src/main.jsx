b6b19288c85ebb37f824e62af284353155a2cb2c
Yermukhamed Shakhman <ermukhamed.shakhman@gmail.com>
2025-11-11 20:59:36 +0100

    fix: gh pages via actions

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import "./styles/globals.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles/globals.css'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// Use HashRouter when hosted under a subpath (GitHub Pages)
+const isSubpath = import.meta.env.BASE_URL !== '/'
+const Router = isSubpath ? HashRouter : BrowserRouter
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <Router basename={import.meta.env.BASE_URL}>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+    </Router>
+  </React.StrictMode>
+)
