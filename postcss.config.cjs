2d4539e9bd2d38f5b2e8a479a467462f737cbcf3
Yermukhamed Shakhman <ermukhamed.shakhman@gmail.com>
2025-11-11 20:36:47 +0100

    fix: try gh pages via actions

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,1 +1,6 @@
-module.exports = { plugins: [require('@tailwindcss/postcss')] };
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // include all component and page files
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
